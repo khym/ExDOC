80bf12659208ce7c148f563503c18009d29442d6
ittipat kakulphimp <ittipatkhym@gmail.com>
2017-06-06 09:02:38 +0700

    refactor code

--- ExDoc/Controllers/IssueController.cs
+++ ExDoc/Controllers/IssueController.cs
@@ -33,15 +33,31 @@ namespace ExDoc.Controllers
             return PartialView();
         }
 
-        public void Save2DB(string id, string path, string uploadby)
+        public void File2DB(string issue_no, HttpPostedFileBase[] doc_file, string cust_no)
         {
-            using (var db = new EX_DOCEntities())
+
+            var indy_cust = new Relation_Issue_Cust();
+
+            indy_cust.cust_no = cust_no;
+            indy_cust.issue_id = issue_no;
+            ex_doc.Relation_Issue_Cust.Add(indy_cust);
+
+            foreach (var doc in doc_file)
             {
-                DocFile file = new DocFile();
-                file.file_name = path;
-                file.issue_id = id;
-                db.DocFile.Add(file);
-                db.SaveChanges();
+                string subPath = "~/UploadFiles/" + cust_no + "/";
+                if (!Directory.Exists(Server.MapPath(subPath)))
+                {
+                    Directory.CreateDirectory(Server.MapPath(subPath));
+                }
+
+                var fileName = DateTime.Now.Millisecond + "_" + Path.GetFileName(doc.FileName.Replace('#', ' ').Replace('%', ' '));
+                var path = Path.Combine(Server.MapPath(subPath), fileName);
+                doc.SaveAs(path);
+
+                var filedb = new DocFile();
+                filedb.issue_id = issue_no;
+                filedb.file_name = subPath + fileName;
+                ex_doc.DocFile.Add(filedb);
             }
         }
 
@@ -62,96 +78,40 @@ namespace ExDoc.Controllers
             save_issue.rec_date = d;
             save_issue.tnc_product = tnc_product;
             save_issue.doc_type_id = 1;
+            save_issue.issue_date = DateTime.Now;
             ex_doc.Issue.Add(save_issue);
-            //ex_doc.SaveChanges();
-
-            
-   //Contains
-
-            //for (int i = 0; i < customer.Length; i++)
-            //{
-            //    string cust_id = customer[i];
-            //    //if (customer_type == "Customer Number")
-            //    //{
-            //       //var sql =  ex_doc.Customer.Where(w => w.cust_name.Contains(item)).Select(a=>a.cust_no);
-            //    //    cust_id = customer[i];
-            //    //}
-            //    //else
-            //        if (customer_type == "Customer Name")
-            //    {
-            //        //var sql = ex_doc.Customer.Where(w => w.cust_name.Contains(customer[i])).Select(a => a.cust_no).ToArray();
-            //        var sql = (from a in ex_doc.Customer where a.cust_name.Contains(customer[i]) select a.cust_no);
-
-            //        //cust_id = sql[i];
-
-
-                                            
-            //    }
-
-            //    save_cust.cust_no = cust_id;
-            //    save_cust.issue_id = issue_no;
-            //    ex_doc.Relation_Issue_Cust.Add(save_cust);
-            //    ex_doc.SaveChanges();
-
-            //    foreach (var doc in doc_file)
-            //    {
-            //        string subPath = "~/UploadFiles/" + cust_id + "/";
-            //        if (!Directory.Exists(Server.MapPath(subPath)))
-            //        {
-            //            Directory.CreateDirectory(Server.MapPath(subPath));
-            //        }
-
-            //        var fileName = DateTime.Now.Millisecond + "_" + Path.GetFileName(doc.FileName.Replace('#', ' ').Replace('%', ' '));
-            //        var path = Path.Combine(Server.MapPath(subPath), fileName);
-            //        doc.SaveAs(path);
-            //        file2db.file_name = subPath + fileName;
-            //        file2db.issue_id = issue_no;
-            //        ex_doc.DocFile.Add(file2db);
-            //        ex_doc.SaveChanges();
-            //    }
-            //}
 
             foreach (var item in customer)
             {
 
                 if (customer_type == "Customer Number")
                 {
-                    //var sql =  ex_doc.Customer.Where(w => w.cust_name.Contains(item)).Select(a=>a.cust_no);
 
-                    //save_cust.cust_no = item;
-                    //save_cust.issue_id = issue_no;
-                    //ex_doc.Relation_Issue_Cust.Add(save_cust);
+                   //var indy_cust = new Relation_Issue_Cust();
 
-                   var indy_cust = new Relation_Issue_Cust();
-                   indy_cust.cust_no = item;
-                   indy_cust.issue_id = issue_no;
-                   ex_doc.Relation_Issue_Cust.Add(indy_cust);
+                   //indy_cust.cust_no = item;
+                   //indy_cust.issue_id = issue_no;
+                   //ex_doc.Relation_Issue_Cust.Add(indy_cust);
 
-                    //ex_doc.SaveChanges();
+                   File2DB(issue_no,doc_file,item);
 
-                    foreach (var doc in doc_file)
-                    {
-                        string subPath = "~/UploadFiles/" + item + "/";
-                        if (!Directory.Exists(Server.MapPath(subPath)))
-                        {
-                            Directory.CreateDirectory(Server.MapPath(subPath));
-                        }
+                    //foreach (var doc in doc_file)
+                    //{
+                    //    string subPath = "~/UploadFiles/" + item + "/";
+                    //    if (!Directory.Exists(Server.MapPath(subPath)))
+                    //    {
+                    //        Directory.CreateDirectory(Server.MapPath(subPath));
+                    //    }
 
-                        var fileName = DateTime.Now.Millisecond + "_" + Path.GetFileName(doc.FileName.Replace('#', ' ').Replace('%', ' '));
-                        var path = Path.Combine(Server.MapPath(subPath), fileName);
-                        doc.SaveAs(path);
-
-                        
-                        //file2db.file_name = subPath + fileName;
-                        //file2db.issue_id = issue_no;
-                        //ex_doc.DocFile.Add(file2db);
-                        //ex_doc.SaveChanges();
-
-                        var filedb = new DocFile();
-                        filedb.issue_id = issue_no;
-                        filedb.file_name = subPath + fileName;
-                        ex_doc.DocFile.Add(filedb);
-                    }
+                    //    var fileName = DateTime.Now.Millisecond + "_" + Path.GetFileName(doc.FileName.Replace('#', ' ').Replace('%', ' '));
+                    //    var path = Path.Combine(Server.MapPath(subPath), fileName);
+                    //    doc.SaveAs(path);
+                 
+                    //    var filedb = new DocFile();
+                    //    filedb.issue_id = issue_no;
+                    //    filedb.file_name = subPath + fileName;
+                    //    ex_doc.DocFile.Add(filedb);
+                    //}
 
 
                 }
@@ -162,72 +122,38 @@ namespace ExDoc.Controllers
                     foreach (var item2 in sql)
                     {
 
+                        //var indy_cust = new Relation_Issue_Cust();
 
-                        //save_cust.cust_no = item2;
-                        //save_cust.issue_id = issue_no;
-                        //ex_doc.Relation_Issue_Cust.Add(save_cust);
+                        //indy_cust.cust_no = item2;
+                        //indy_cust.issue_id = issue_no;
+                        //ex_doc.Relation_Issue_Cust.Add(indy_cust);
 
-                        var indy_cust = new Relation_Issue_Cust();
-                        indy_cust.cust_no = item2;
-                        indy_cust.issue_id = issue_no;
-                        ex_doc.Relation_Issue_Cust.Add(indy_cust);
-                        
+                        File2DB(issue_no, doc_file, item2);
 
-                        foreach (var doc in doc_file)
-                        {
-                            string subPath = "~/UploadFiles/" + item2 + "/";
-                            if (!Directory.Exists(Server.MapPath(subPath)))
-                            {
-                                Directory.CreateDirectory(Server.MapPath(subPath));
-                            }
+                        //foreach (var doc in doc_file)
+                        //{
+                        //    string subPath = "~/UploadFiles/" + item2 + "/";
+                        //    if (!Directory.Exists(Server.MapPath(subPath)))
+                        //    {
+                        //        Directory.CreateDirectory(Server.MapPath(subPath));
+                        //    }
 
-                            var fileName = DateTime.Now.Millisecond + "_" + Path.GetFileName(doc.FileName.Replace('#', ' ').Replace('%', ' '));
-                            var path = Path.Combine(Server.MapPath(subPath), fileName);
-                            doc.SaveAs(path);
-                            //file2db.file_name = subPath + fileName;
-                            //file2db.issue_id = issue_no;
-                            //ex_doc.DocFile.Add(file2db);
+                        //    var fileName = DateTime.Now.Millisecond + "_" + Path.GetFileName(doc.FileName.Replace('#', ' ').Replace('%', ' '));
+                        //    var path = Path.Combine(Server.MapPath(subPath), fileName);
+                        //    doc.SaveAs(path);
 
-                            var filedb = new DocFile();
-                            filedb.issue_id = issue_no;
-                            filedb.file_name = subPath + fileName;
-                            ex_doc.DocFile.Add(filedb);
-                            
-                        }
+                        //    var filedb = new DocFile();
+                        //    filedb.issue_id = issue_no;
+                        //    filedb.file_name = subPath + fileName;
+                        //    ex_doc.DocFile.Add(filedb);
+
+                        //}
                     }
-                    //ex_doc.SaveChanges();
                 }
                 
             }
 
             ex_doc.SaveChanges();
-
-
-            
-
-
-
-            // Add data to DB TD_File //
-            //string subPath = "~/UploadFiles/" + dt.Year + "/" + dt.Month + "/" + main_id + "/";
-            //if (!Directory.Exists(Server.MapPath(subPath)))
-            //{
-            //    Directory.CreateDirectory(Server.MapPath(subPath));
-            //}
-            //foreach (var file in files)
-            //{
-            //    if (file != null && file.ContentLength > 0)
-            //    {
-            //        if (file.ContentType == "application/pdf")//Check accept file type.
-            //        {
-            //            var fileName = DateTime.Now.Millisecond + "_" + Path.GetFileName(file.FileName.Replace('#', ' ').Replace('%', ' '));
-            //            var path = Path.Combine(Server.MapPath(subPath), fileName);
-            //            file.SaveAs(path);
-            //            SaveFiletoDB(main_id, subPath + fileName, Session["SA_Auth"].ToString());
-            //        }
-            //    }
-            //}
-
-
             return RedirectToAction("Index", "Issue");
         }
 
